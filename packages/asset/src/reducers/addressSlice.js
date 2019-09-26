fba6c13ff4444cdc3e7579bc0b2bfe36cd7964f9
Yongfeng LI <wliyongfeng@gmail.com>
2019-09-26 11:38:37 +0800

    Set default address to the demo one

--- packages/asset/src/reducers/addressSlice.js
+++ packages/asset/src/reducers/addressSlice.js
@@ -3,8 +3,8 @@ import { createSlice } from 'redux-starter-kit'
 const addressSlice = createSlice({
   slice: 'address',
   initialState: {
-    name: 'mine',
-    address: '5QtntcJYGdnQ5dNfZrAqBNfJbvSw7u7akvzwxCEzUhL1JibT'
+    name: 'demo',
+    address: '5TGy4d488i7pp3sjzi1gibqFUPLShddfk7qPY2S445ErhDGq'
   },
   reducers: {}
 })
