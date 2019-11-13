cbc6ebb3dca52884d04a4a9727cada33aca62d89
Yongfeng LI <wliyongfeng@gmail.com>
2019-11-13 14:45:42 +0800

    Set api default to mainnet api

--- packages/wallet/src/services/api.js
+++ packages/wallet/src/services/api.js
@@ -1,7 +1,7 @@
-let api = null
-
 export const mainNetApi = 'https://api.chainx.org/'
 export const testNetApi = 'https://testnet.api.chainx.org.cn/'
+
+let api = mainNetApi
 
 export const setApi = url => {
   api = url
