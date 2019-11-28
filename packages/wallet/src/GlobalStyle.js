aeb172c405dc26604ee884bac47f08e97789e554
Yongfeng LI <wliyongfeng@gmail.com>
2019-11-28 17:24:26 +0800

    Extract GlobalStyle

---
+++ packages/wallet/src/GlobalStyle.js
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+html {
+  height: 100%;
+}
+
+body {
+  margin: 0 auto;
+  height: 100%;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background: #F0F1F2;
+  overflow-x: auto;
+
+  #root {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    & > div.wrapper {
+      display: flex;
+      flex: 1;
+      overflow-y: auto;
+
+      & > div {
+        &:not(.staking) {
+          padding-top: 16px;
+          margin: 0 auto;
+          min-width: 1280px;
+          max-width: 1440px;
+        }
+      }
+    }
+  }
+}
+
+h6, p {
+  margin: 0;
+}
+
+ul, li {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+`
+
+export default GlobalStyle
--- packages/wallet/src/index.js
+++ packages/wallet/src/index.js
@@ -5,7 +5,6 @@ import * as serviceWorker from './serviceWorker'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers'
-import { createGlobalStyle } from 'styled-components'
 import { getChainx, setChainx } from './services/chainx'
 import { setAccount, setExtensionAccounts } from './reducers/addressSlice'
 import { setNode } from './reducers/nodeSlice'
@@ -14,55 +13,7 @@ import { setNetwork } from './reducers/settingsSlice'
 import { mainNetApi, setApi, testNetApi } from './services/api'
 import { mainNetDemoAccount, testNetDemoAccount } from './utils/constants'
 import { isDemoAccount } from './selectors'
-
-const GlobalStyle = createGlobalStyle`
-html {
-  height: 100%;
-}
-
-body {
-  margin: 0 auto;
-  height: 100%;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  background: #F0F1F2;
-  overflow-x: auto;
-
-  #root {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-
-    & > div.wrapper {
-      display: flex;
-      flex: 1;
-      overflow-y: auto;
-
-      & > div {
-        &:not(.staking) {
-          padding-top: 16px;
-          margin: 0 auto;
-          min-width: 1280px;
-          max-width: 1440px;
-        }
-      }
-    }
-  }
-}
-
-h6, p {
-  margin: 0;
-}
-
-ul, li {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-`
+import GlobalStyle from './GlobalStyle'
 
 function loadState() {
   try {
