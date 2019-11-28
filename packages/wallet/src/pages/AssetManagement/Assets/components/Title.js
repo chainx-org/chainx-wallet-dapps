5c5753c5c0cce6ba0ee87bb7d46132cab65686b7
Yongfeng LI <wliyongfeng@gmail.com>
2019-11-28 14:58:39 +0800

    Fix asset view title font

--- packages/wallet/src/pages/AssetManagement/Assets/components/Title.js
+++ packages/wallet/src/pages/AssetManagement/Assets/components/Title.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export default styled.h6`
   opacity: 0.32;
-  font-size: 14px;
+  font-size: 12px;
   color: #000000;
-  line-height: 20px;
-  font-weight: 400;
+  letter-spacing: 0.2px;
+  line-height: 16px;
 `
