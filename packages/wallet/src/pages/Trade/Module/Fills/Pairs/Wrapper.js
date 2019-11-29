270ca23226cf0f8abf563b24126e83c49a5b26d2
Yongfeng LI <wliyongfeng@gmail.com>
2019-11-29 15:08:57 +0800

    Add trade pairs

--- packages/wallet/src/pages/Trade/Module/Fills/Pairs/Wrapper.js
+++ packages/wallet/src/pages/Trade/Module/Fills/Pairs/Wrapper.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import SideCard from '../../../components/SideCard'
+import { TableCell } from '@chainx/ui'
 
 const Wrapper = styled(SideCard)`
   padding: 16px 0;
@@ -28,6 +29,38 @@ export const Header = styled.header`
       }
     }
   }
+`
+
+export const Cell = styled(TableCell)`
+  height: 24px;
+  padding: 0 12px !important;
+  opacity: 0.72;
+  font-weight: 600 !important;
+  font-size: 12px !important;
+  color: #000000;
+  letter-spacing: 0.2px;
+  line-height: 16px;
+`
+
+export const SymbolCell = styled(TableCell)`
+  opacity: 0.72;
+  font-size: 14px !important;
+  font-weight: 600 !important;
+  color: #000000;
+  letter-spacing: 0.12px;
+  line-height: 20px;
+  border-bottom: 0 !important;
+  width: 50%;
+`
+
+export const PriceCell = styled(TableCell)`
+  text-align: right !important;
+  font-weight: 600 !important;
+  font-size: 14px !important;
+  color: #dc6e46 !important;
+  line-height: 20px;
+  border-bottom: 0 !important;
+  width: 50%;
 `
 
 export default Wrapper
--- packages/wallet/src/pages/Trade/Module/Fills/Pairs/index.js
+++ packages/wallet/src/pages/Trade/Module/Fills/Pairs/index.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
-import Wrapper, { Header } from './Wrapper'
+import Wrapper, { Cell, Header, PriceCell, SymbolCell } from './Wrapper'
 import { useSelector } from 'react-redux'
 import { currenciesSelector } from './selectors'
+import { Table, TableBody, TableHead, TableRow } from '@chainx/ui'
+import { pairsSelector } from '../../../../../reducers/tradeSlice'
+import { toPrecision } from '../../../../../utils'
 
 export default function() {
   const currencies = useSelector(currenciesSelector)
+  const pairs = useSelector(pairsSelector)
 
   const [activeCurrencyIndex, setActiveCurrencyIndex] = useState(0)
+  const nowCurrency = currencies[activeCurrencyIndex]
+  const targetPairs = pairs.filter(pair => pair.currency === nowCurrency)
 
   return (
     <Wrapper>
@@ -25,6 +31,31 @@ export default function() {
           })}
         </ul>
       </Header>
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <Cell>币种</Cell>
+            <Cell style={{ textAlign: 'right' }}>价格</Cell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {targetPairs.map((pair, index) => {
+            const precision = pair.precision
+            const showPrecision = precision - pair.unitPrecision
+            return (
+              <TableRow key={index}>
+                <SymbolCell>{pair.assets}</SymbolCell>
+                <PriceCell>
+                  {Number(toPrecision(pair.lastPrice, precision)).toFixed(
+                    showPrecision
+                  )}
+                </PriceCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
     </Wrapper>
   )
 }
