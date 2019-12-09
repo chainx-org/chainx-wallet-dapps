7ec933aac92d525d7ef9890b052df8c09ccfd22b
Yongfeng LI <wliyongfeng@gmail.com>
2019-12-09 11:30:12 +0800

    Fix vote dialog

--- packages/wallet/src/reducers/runStatusSlice.js
+++ packages/wallet/src/reducers/runStatusSlice.js
@@ -4,7 +4,8 @@ const runStatusSlice = createSlice({
   name: 'runStatus',
   initialState: {
     unNominateOpen: false,
-    switchNominationOpen: false
+    switchNominationOpen: false,
+    voteOpen: false
   },
   reducers: {
     setUnNominateOpen(state, action) {
@@ -12,17 +13,22 @@ const runStatusSlice = createSlice({
     },
     setSwitchNominationOpen(state, action) {
       state.switchNominationOpen = action.payload
+    },
+    setVoteOpen(state, action) {
+      state.voteOpen = action.payload
     }
   }
 })
 
 export const {
   setUnNominateOpen,
-  setSwitchNominationOpen
+  setSwitchNominationOpen,
+  setVoteOpen
 } = runStatusSlice.actions
 
 export const unNominateOpenSelector = state => state.runStatus.unNominateOpen
 export const switchNominationOpenSelector = state =>
   state.runStatus.switchNominationOpen
+export const voteOpenSelector = state => state.runStatus.voteOpen
 
 export default runStatusSlice.reducer
