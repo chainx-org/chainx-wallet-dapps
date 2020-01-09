bb73eb35ebb77365f1b05def861f9f74bde0dd5e
Yongfeng LI <wliyongfeng@gmail.com>
2020-01-09 16:52:19 +0800

    Extract AccountList Wrapper

--- packages/wallet/src/pages/Header/AccountList/index.js
+++ packages/wallet/src/pages/Header/AccountList/index.js
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react'
-import styled from 'styled-components'
 import $t from '../../../locale'
 import { useDispatch, useSelector } from 'react-redux'
 import {
@@ -14,95 +13,7 @@ import {
   mainNetDemoAccount,
   testNetDemoAccount
 } from '../../../utils/constants'
-
-const Wrapper = styled.ul`
-  position: absolute;
-  top: 56px;
-  right: 16px;
-  padding: 16px;
-
-  width: 300px;
-
-  background: rgba(255, 255, 255, 1);
-  border: 1px solid #dce0e2;
-  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.08), 0 8px 8px 0 rgba(0, 0, 0, 0.16);
-  border-radius: 10px;
-
-  z-index: 99;
-
-  & > li {
-    cursor: pointer;
-    &:not(:first-of-type) {
-      padding-top: 10px;
-      border-top: 1px solid #eee;
-    }
-    &:not(:last-of-type) {
-      padding-bottom: 10px;
-    }
-
-    h4 {
-      display: flex;
-      justify-content: space-between;
-
-      margin: 0;
-      font-size: 13px;
-      color: #000000;
-      letter-spacing: 0.2px;
-      line-height: 18px;
-
-      span {
-        opacity: 0.72;
-      }
-
-      span.extension {
-        background: #f6c94a;
-        border: 1px solid rgba(0, 0, 0, 0.04);
-        border-radius: 10px;
-
-        padding: 0 15px;
-
-        font-size: 12px;
-        font-weight: 500;
-        line-height: 16px;
-      }
-    }
-    p {
-      margin-top: 8px;
-      opacity: 0.32;
-      font-size: 12px;
-      color: #000000;
-      letter-spacing: 0.2px;
-      line-height: 16px;
-
-      overflow-x: hidden;
-      text-overflow: ellipsis;
-    }
-
-    &.extension {
-      display: flex;
-      flex-direction: row-reverse;
-      a {
-        background: #f6c94a;
-        border: 1px solid rgba(0, 0, 0, 0.04);
-        border-radius: 8px;
-
-        display: inline-flex;
-        padding: 6px;
-        align-items: center;
-        text-decoration: none;
-
-        span {
-          margin-left: 12px;
-          opacity: 0.72;
-          font-size: 14px;
-          color: #000000;
-          letter-spacing: 0.12px;
-          line-height: 20px;
-        }
-      }
-    }
-  }
-`
+import Wrapper from './Wrapper'
 
 export default function({ close = noneFunc }) {
   const extensionAccounts = useSelector(extensionAccountsSelector)
