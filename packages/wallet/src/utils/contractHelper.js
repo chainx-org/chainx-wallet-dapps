5f3d0f96d8867d5d90f1266f972fcf0709d13972
Yongfeng LI <wliyongfeng@gmail.com>
2020-05-08 12:17:10 +0800

    Refactor some contract helper functions

--- packages/wallet/src/utils/contractHelper.js
+++ packages/wallet/src/utils/contractHelper.js
@@ -1,6 +1,5 @@
 import { getChainx } from '../services/chainx'
-import { compactAddLength, stringCamelCase, u8aToU8a } from '@chainx/util'
-import { blake2AsU8a } from '@chainx/util-crypto'
+import { stringCamelCase, u8aToU8a } from '@chainx/util'
 import { createType } from '@chainx/types'
 import { Abi } from '@chainx/api-contract'
 import { littleEndianToBigEndian } from './index'
@@ -16,18 +15,6 @@ import { showSnack, signAndSendExtrinsic } from './chainxProvider'
 // Bob:     0x3b7b60af2abcd57ba401ab398f84f4ca54bd6b2140d2503fbcf3286535fe3ff1
 // Charlie: 0x072c02fa1409dc37e03a4ed01703d4a9e6bba9c228a49a00366e9630a97cba7c
 // Dave:    0x771f47d3caf8a2ee40b0719e1c1ecbc01d73ada220cf08df12a00453ab703738
-
-let chainx
-// window.chainxProvider && window.chainxProvider.getCurrentNode().then(async node => {
-//   chainx = await setChainx(node.url)
-// })
-
-const Alice =
-  chainx &&
-  chainx.account.from(
-    '0x436861696e582d5361746f736869202020202020202020202020202020202020'
-  )
-const enableExtension = true
 
 export async function call(abi, address, method, gas, params) {
   const accountAddress = addressSelector(store.getState())
@@ -106,51 +93,18 @@ export async function send(abi, address, method, params, value, gas) {
 export async function isCodeHashExist(codeHash) {
   const chainx = getChainx()
   const result = await chainx.api.query.xContracts.pristineCode(codeHash)
-  if (result.length > 0) {
-    return true
-  } else {
-    return false
-  }
+  return result.length > 0
 }
 
 export async function isContractExist(address) {
   const chainx = getChainx()
   try {
     const result = await chainx.api.query.xContracts.contractInfoOf(address)
-    if (result.isEmpty) {
-      return false
-    } else {
-      return true
-    }
+    return !result.isEmpty
   } catch (error) {
     console.log(error)
     return false
   }
-}
-
-export async function uploadContract(file, gas, cb) {
-  const chainx = getChainx()
-  const codehash = blake2AsU8a(file.data).reduce((result, i) => {
-    return result + ('0' + i.toString(16)).slice(-2)
-  }, '0x')
-  const isExist = await isCodeHashExist(codehash)
-  if (isExist) {
-    cb({
-      err: {
-        message:
-          'codehash exist, please add existing codehash, using: ' + codehash
-      },
-      status: ''
-    })
-    return
-  }
-  const args = [gas, compactAddLength(file.data)]
-  const ex = chainx.api.tx.xContracts['putCode'](...args)
-  if (enableExtension) {
-    await contractApi(ex.toHex(), cb)
-    return
-  }
-  ex.signAndSend(Alice, convertCb(cb))
 }
 
 export async function deploy(_abi, params, endowment, gas) {
@@ -172,19 +126,12 @@ export async function deploy(_abi, params, endowment, gas) {
   console.log('deploy abi in utils ', abi, args, params)
   const ex = chainx.api.tx.xContracts['instantiate'](...args)
   const accountAddress = addressSelector(store.getState())
-  const status = await signAndSendExtrinsic(accountAddress, ex.toHex())
-  return status
+  return signAndSendExtrinsic(accountAddress, ex.toHex())
 }
 
 async function signAndSend(hex) {
   const accountAddress = addressSelector(store.getState())
-  const status = await signAndSendExtrinsic(accountAddress, hex)
-  return status
-}
-
-const contractApi = async (hex, cb) => {
-  const account = await window.chainxProvider.enable()
-  window.chainxProvider.signAndSendExtrinsic(account.address, hex, cb)
+  return signAndSendExtrinsic(accountAddress, hex)
 }
 
 const parseParams = (args, params) => {
@@ -197,10 +144,4 @@ const parseParams = (args, params) => {
     }
   })
   return params
-}
-
-const convertCb = cb => {
-  return (err, result) => {
-    cb({ err: err, status: result })
-  }
 }
